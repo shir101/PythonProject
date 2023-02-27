edec95a5b5b7ada5d3ebfceae00e4d12d0fa5d72
Shir Siman Tov <49974136+shir101@users.noreply.github.com>
2023-02-27 02:34:32 +0200

    Update fasta_downloader.py
    update number 2

--- fasta_downloader.py
+++ fasta_downloader.py
@@ -2,20 +2,20 @@ from DataCollector import Covid19DataPortalAccessionFetcher as cdc
 import shutil
 import os
 
-#dc = cdc()  # instance of the data collector
+dc = cdc()  # instance of the data collector
 print("dc cdc successful!!!!!")
-#avail_lineages = dc.getLineagesList()  ####
-avail_lineages = ['B.1.1', 'B.1.1.285', 'B.12', 'B.1.1.284', 'B.1.1.214']
+avail_lineages = dc.getLineagesList()  ####
+#avail_lineages = ['B.1.1', 'B.1.1.285', 'B.12', 'B.1.1.284', 'B.1.1.214']
 # print(avail_lineages)
 source = "./data/raw/"
-dest = "./data/"
+#dest = "./data/"
 
 for lineage in avail_lineages[1:3]:
     try:
         os.mkdir("./data/"+lineage)
         dest2 = "./data/"+lineage
-        # accessions = dc.getAccessionsByLineage(lineage)  #### take accessions of a certain lineage
-        # dc.downloadAccessions(accessions)
+        accessions = dc.getAccessionsByLineage(lineage)  #### take accessions of a certain lineage
+        dc.downloadAccessions(accessions)
         files = os.listdir(source)
         for file in files:
             shutil.move(os.path.join(source,file),dest2)
